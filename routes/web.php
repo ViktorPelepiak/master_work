8a89c070e3479ae78186844b18bead77c3b99a09
ViktorPelepiak <viktorpelepiak@gmail.com>
2021-11-21 14:25:47 +0200

    update roting

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ class KeyValues
     public static function getKeyValues(): array
     {
         return [
-            'root_path' => 'http://localhost:8000'
+//            'root_path' => 'http://localhost:8000'
+            'root_path' => env('APP_URL')
         ];
     }
 }
